d6dff683ded48c38c3429492c2fbe8627a8df64b
BlazerXXI <ruslandendik@gmail.com>
2024-11-21 18:21:52 +0200

    add themeClasses constant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,28 +8,29 @@ interface IButton {
 	children: React.ReactNode;
 }
 
-const Button = (props: IButton) => {
-	const {
-		type = "button",
-		className,
-		theme = "blue",
-		onClick,
-		children,
-	} = props;
+const Button: React.FC<IButton> = ({
+	type = "button",
+	className,
+	theme = "blue",
+	onClick,
+	children,
+}) => {
+	const themeClasses = {
+		blue: "bg-blue-500 hover:bg-blue-600 text-white",
+		red: "bg-red-500 hover:bg-red-600 text-white",
+		green: "bg-green-500 hover:bg-green-600 text-white",
+		orange: "bg-orange-500 hover:bg-orange-600 text-white",
+		gray: "bg-gray-300 hover:bg-gray-400 text-black",
+		white: "bg-white hover:bg-gray-100 text-black",
+		black: "bg-black hover:bg-gray-800 text-white",
+	};
+
 	return (
 		<button
 			type={type}
 			className={cx(
 				"px-4 py-2 rounded transition duration-200 flex items-center justify-center",
-				{
-					"bg-blue-500 hover:bg-blue-600 text-white": theme === "blue",
-					"bg-red-500 hover:bg-red-600 text-white": theme === "red",
-					"bg-green-500 hover:bg-green-600 text-white": theme === "green",
-					"bg-orange-500 hover:bg-orange-600 text-white": theme === "orange",
-					"bg-gray-300 hover:bg-gray-400 text-black": theme === "gray",
-					"bg-white hover:bg-gray-100 text-black": theme === "white",
-					"bg-black hover:bg-gray-800 text-white": theme === "black",
-				},
+				themeClasses[theme],
 				className
 			)}
 			onClick={onClick}
@@ -40,3 +41,4 @@ const Button = (props: IButton) => {
 };
 
 export default Button;
+
